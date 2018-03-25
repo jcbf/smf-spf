5a6af2b98e28d096d09c55f46b5ffc1f7d84ff53
jcbf <underspell@gmail.com>
2018-03-25 16:28:21 +0200

    Treat NONE adn INVALID the same way

--- smf-spf.c
+++ smf-spf.c
@@ -788,7 +788,7 @@ static sfsistat smf_envfrom(SMFICTX *ctx, char **args) {
     SPF_request_set_env_from(spf_request, context->sender);
     if (SPF_request_query_mailfrom(spf_request, &spf_response)) {
 	syslog(LOG_INFO, "SPF none: ip=%s, fqdn=%s, helo=%s, from=%s", context->addr, context->fqdn, context->helo, context->from);
-    if (status == SPF_RESULT_NONE) {
+    if ((status == SPF_RESULT_NONE) || (status == SPF_RESULT_INVALID)) {
             if (conf.refuse_none && !strstr(context->from, "<>")) {
                     char reject[2 * MAXLINE];
                     snprintf(reject, sizeof(reject), "Sorry, we only accept mail from SPF enabled domains", context->sender);
