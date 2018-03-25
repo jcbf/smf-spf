d3b35ab382a905edfc46ff799097c22411c97a08
jcbf <underspell@gmail.com>
2018-03-25 17:40:14 +0200

    Test invalid sender and to with hard reject

---
+++ tests/03-invalid-sender-hardfail.lua
@@ -0,0 +1,29 @@
+mt.echo("Invalid sender ")
+
+-- try to start the filter
+mt.startfilter("./smf-spf", "-f", "-c","./smf-spf-tests-refuse.conf")
+
+-- try to connect to it
+conn = mt.connect("inet:2424@127.0.0.1", 40, 0.25)
+if conn == nil then
+	error("mt.connect() failed")
+end
+
+-- send connection information
+
+mt.macro(conn, SMFIC_CONNECT, "j", "mta.name.local")
+if mt.conninfo(conn, "a.server.name.local", "10.11.12.13") ~= nil then
+	error("mt.conninfo() failed")
+end
+
+-- send envelope macros and sender data
+-- mt.helo() is called implicitly
+mt.macro(conn, SMFIC_MAIL, "i", "DEADBEAF")
+if mt.mailfrom(conn, "underspell.com") ~= nil then
+        error("mt.mailfrom() failed")
+end
+if mt.getreply(conn) ~= SMFIR_REPLYCODE then
+        error("mt.mailfrom() unexpected reply")
+end
+
+print ("received SMFIR_REPLYCODE ") 
