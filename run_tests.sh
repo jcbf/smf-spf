27f63407ab2f62282a54ff8afb9a18f0e765792c
José Borges Ferreira <jose.ferreira@bitsighttech.com>
2020-05-02 20:55:03 +0100

    Better information from run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 ./smf-spf -h
+RET=0
 for testfile in tests/* ; do
 	echo "#########################################"
 	echo "Running $testfile ..."
 	miltertest  -s $testfile
 	status=$?
 	echo -n "returned "
-	[ $status -eq 0 ] && echo -e "\e[32msuccessful\e[39m" || echo -e "\e[31mfailed\e[39m"
+	if [ $status -eq 0 ] ; then 
+            echo -e "\e[32msuccessful\e[39m"
+    else 
+            echo -e "\e[31mfailed\e[39m"
+            RET=1
+            FAILED+="$testfile "
+    fi
 done
+
+[[ -z "$FAILED+" ]] || echo "Failed tests $FAILED"
+exit $RET
