31c2c0db43e9a38872d7027cdff33656c47d6009
José Borges Ferreira <jose.ferreira@mailspike-technologies.pt>
2020-11-10 09:27:18 +0000

    Removed colored output

--- tests/bin/run_tests.sh
+++ tests/bin/run_tests.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NOCOLOR='\033[0m' 
 ./smf-spf -h
 for conf in ./tests/fail/* /file/does/not/exists ; do
 	echo "#########################################"
@@ -14,5 +11,5 @@ for testfile in tests/0* ; do
 	echo "#########################################"
 	echo "Running $testfile ..."
 	echo "#########################################"
-	miltertest -s $testfile && echo -e "$GREEN Test OK $NOCOLOR" || echo -e "$RED Test failed $NOCOLOR"
+	miltertest -s $testfile && echo -e "Test OK" || echo -e "Test failed"
 done
