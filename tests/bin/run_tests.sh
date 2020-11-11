79e5e8136c891d3b37541822c2b865b5b5f1846d
José Borges Ferreira <jose.ferreira@mailspike-technologies.pt>
2020-11-11 19:25:57 +0000

    Restore some tests

--- tests/bin/run_tests.sh
+++ tests/bin/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ./smf-spf -h
-for conf in ./tests/fail/* /file/does/not/exists ; do
+for conf in ./tests/fail/* ; do
 	echo "#########################################"
 	echo "Running $conf ..."
 	echo "#########################################"
-    #./smf-spf -f -c $conf 2>&1 > /dev/null
+    ./smf-spf -f -c $conf 2>&1 > /dev/null
 done
 for testfile in tests/0* ; do
 	echo "#########################################"
